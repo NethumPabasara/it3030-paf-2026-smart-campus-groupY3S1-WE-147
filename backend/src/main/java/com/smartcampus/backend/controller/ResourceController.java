2ce616c6f0cf3193320218f508f2696a48bd2b8b
pabasara <pabasara.e.g@gmail.com>
2026-04-14 14:10:13 +0530

    setup H2 database and fix JPA entity manager issues

---
+++ backend/src/main/java/com/smartcampus/backend/controller/ResourceController.java
@@ -0,0 +1,23 @@
+package com.smartcampus.backend.controller;
+
+import com.smartcampus.backend.entity.Resource;
+import com.smartcampus.backend.service.ResourceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/resources")
+public class ResourceController {
+
+    private final ResourceService resourceService;
+
+    public ResourceController(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
+
+    @GetMapping
+    public List<Resource> getAllResources() {
+        return resourceService.getAllResources();
+    }
+}
